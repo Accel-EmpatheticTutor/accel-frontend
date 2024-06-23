eaac8ef21056f60e2f140791e00b061fd742898d
ElemelonWind <c.yang.2023@gmail.com>
2024-06-22 19:48:45 -0700

    styling

--- pages/tutor.js
+++ pages/tutor.js
@@ -8,7 +8,7 @@ import Markdown from "react-markdown";
 
 import { useVoice, VoiceReadyState } from "@humeai/voice-react";
 import Microphone from "../components/Microphone";
-import { Hume, HumeClient } from "hume";
+import Expressions from "@/components/Expressions";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +17,9 @@ export default function Tutor() {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   const [quizMode, setQuizMode] = useState(false);
-  // const client = new HumeClient({
-  //     apiKey: process.env.NEXT_PUBLIC_HUME_API_KEY,
-  //     secretKey: process.env.NEXT_PUBLIC_HUME_SECRET_KEY,
-  // });
-  // import messages as audioMessages
+  const [recentEmotions, setRecentEmotions] = useState([]);
+  const [recording, setRecording] = useState(false);
+
   const {
     connect,
     disconnect,
@@ -30,12 +28,16 @@ export default function Tutor() {
   } = useVoice();
 
   useEffect(() => {
+    console.log(recording)
     if (audioMessages.length > 0) {
       setMessage(
-        audioMessages.filter((m) => m.type === "user_message").map((m) => m.message.content).join(" ")
-      )
+        audioMessages
+          .filter((m) => m.type === "user_message")
+          .map((m) => m.message.content)
+          .join(" ")
+      );
     }
-  }, [audioMessages])
+  }, [audioMessages, recording]);
 
   const handleSubmit = () => {
     setLoading(true);
@@ -44,17 +46,17 @@ export default function Tutor() {
     }
     setMessages([...messages, { message, type: "user", emotion: [] }]);
     sendMessage({ message, emotion: [] }).then((data) => {
-        if (data.quiz) {
-            if (quizMode) exitQuizMode();
-            else enterQuizMode();
-        }
-        if (data.message) {
-            setMessages([
-                ...messages,
-                { message, type: "user", emotion: [] },
-                { message: data.message, type: "model" },
-              ]);
-        }
+      if (data.quiz) {
+        if (quizMode) exitQuizMode();
+        else enterQuizMode();
+      }
+      if (data.message) {
+        setMessages([
+          ...messages,
+          { message, type: "user", emotion: [] },
+          { message: data.message, type: "model" },
+        ]);
+      }
       setLoading(false);
     });
     setMessage("");
@@ -76,7 +78,9 @@ export default function Tutor() {
       readyState === VoiceReadyState.OPEN
     ) {
       disconnect();
+        setRecording(false);
       // add up top 3 emotions from each message
+      if (!message) return;
       let emotions = {};
       for (let m of audioMessages.filter((m) => m.type === "user_message")) {
         for (let e of Object.keys(m.models.prosody.scores)) {
@@ -88,22 +92,41 @@ export default function Tutor() {
         }
       }
       // get top 3 emotions
-      let top3 = Object.keys(emotions)
+      let top3keys = Object.keys(emotions)
         .sort((a, b) => emotions[b] - emotions[a])
-        .slice(0, 3);
+        .slice(0, 5);
+
+      // add percentages
+      let top3 = top3keys.map((e) => {
+        return [e, emotions[e]];
+      });
+
+      let sum = top3.reduce((acc, cur) => acc + cur[1], 0);
+      top3 = top3.map((e) => [e[0], e[1] / sum]);
+
+      setRecentEmotions(top3);
+
       // send message
       setMessages([...messages, { message, type: "user", emotion: top3 }]);
-      sendMessage({ message, emotion: top3 }).then((botMessage) => {
-        setMessages([
-          ...messages,
-          { message, type: "user", emotion: top3 },
-          { message: botMessage, type: "model" },
-        ]);
+      sendMessage({ message, emotion: top3 }).then((data) => {
+        if (data.quiz) {
+          if (quizMode) exitQuizMode();
+          else enterQuizMode();
+        }
+        if (data.message) {
+          setMessages([
+            ...messages,
+            { message, type: "user", emotion: top3 },
+            { message: data.message, type: "model" },
+          ]);
+        }
         setLoading(false);
-        setMessage("");
       });
+      setMessage("");
     } else {
-      connect().then(() => {});
+      connect().then(() => {
+        setRecording(true)
+      });
     }
   };
 
@@ -129,101 +152,132 @@ export default function Tutor() {
     >
       {/* container in the middle vertically + horizontally */}
       <div className="flex flex-col items-center justify-center h-screen w-screen">
-        <h1 className="mb-4 text-transparent text-4xl bg-clip-text bg-gradient-to-br from-sky-200 to-blue-600">
-          Accel
-        </h1>
-        <div className="mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-          Quiz Mode
-        </div>
-        <label className="inline-flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            value=""
-            className="sr-only peer"
-            onChange={(e) => {
-              if (e.target.checked) {
-                enterQuizMode();
-              } else {
-                exitQuizMode();
-              }
-            }}
-            // if quizMode is true, set the checkbox to checked
-            checked={quizMode}
-          />
-          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gradient-to-br from-sky-200 to-blue-600"></div>
-        </label>
-        <div className="h-2/3 w-2/3">
-          <ScrollContainer>
-            {messages.map((m, i) => {
-              if (m.type == "model") {
-                return (
-                  <div
-                    className={
-                      "flex mb-4 cursor-pointer mx-4" + (i == 0 ? " mt-4" : "")
-                    }
-                    key={i}
-                  >
-                    <div className="flex max-w-full bg-white rounded-lg py-2 px-4 gap-3">
-                      <div className="text-gray-700">
-                        <Markdown className="divrose">{m.message}</Markdown>
+        <div className="w-full h-full grid grid-cols-4">
+          <div className="col-span-3 flex flex-col items-center justify-center">
+            <div className="absolute top-0 left-0 w-3/4 p-8 flex flex-col items-center">
+            <h1
+              className="mb-4 text-transparent text-4xl bg-clip-text bg-gradient-to-br from-sky-200 to-blue-600"
+              onClick={() => Router.push("/")}
+            >
+              Accel
+            </h1>
+            <div className="mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Quiz Mode
+            </div>
+            <label className="inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    enterQuizMode();
+                  } else {
+                    exitQuizMode();
+                  }
+                }}
+                // if quizMode is true, set the checkbox to checked
+                checked={quizMode}
+              />
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gradient-to-br from-sky-200 to-blue-600"></div>
+            </label>
+            </div>
+            <div className="absolute bottom-0 left-0 w-3/4 p-8 h-5/6 pb-28">
+              <ScrollContainer>
+                {messages.map((m, i) => {
+                  if (m.type == "model") {
+                    return (
+                      <div
+                        className={
+                          "flex mb-4 cursor-pointer mx-4" +
+                          (i == 0 ? " mt-4" : "")
+                        }
+                        key={i}
+                      >
+                        <div className="flex max-w-full bg-white rounded-lg py-2 px-4 gap-3">
+                          <div className="text-gray-700">
+                            <Markdown className="divrose">{m.message}</Markdown>
+                          </div>
+                        </div>
                       </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        className={
+                          "flex mb-4 justify-end cursor-pointer mx-4" +
+                          (i == 0 ? " mt-4" : "")
+                        }
+                        key={i}
+                      >
+                        <div className="max-w-96 bg-transparent border-white border text-white rounded-lg py-2 px-4 gap-3">
+                          <div className="w-full text-wrap break-words">
+                            {m.message}
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
+                {loading && (
+                  <div className="flex mb-4 cursor-pointer mx-4">
+                    <div className="flex max-w-96 bg-white rounded-lg px-4 py-4 gap-3">
+                      <TypingAnimation />
                     </div>
                   </div>
-                );
-              } else {
-                return (
-                  <div
-                    className={
-                      "flex mb-4 justify-end cursor-pointer mx-4" +
-                      (i == 0 ? " mt-4" : "")
+                )}
+              </ScrollContainer>
+            </div>
+            <footer className="fixed p-0 absolute bottom-0 h-20 flex items-center p-8 w-3/4 rounded-br-lg">
+              <div className="flex items-center p-4 items-center w-full">
+                <input
+                  type="text"
+                  placeholder="Type a message..."
+                  className="w-full p-2 rounded-md border border-white bg-transparent text-white"
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      handleSubmit();
                     }
-                    key={i}
-                  >
-                    <div className="flex max-w-96 bg-transparent border-white border text-white rounded-lg py-2 px-4 gap-3 text-wrap break-words">
-                      <p className="w-full">{m.message}</p>
+                  }}
+                />
+                <button
+                  className={
+                    "bg-transparent border-white border text-white text-md px-4 py-2 ml-2 rounded-lg hover:bg-white hover:text-black duration-500" +
+                    (recording
+                      ? " bg-red-600 border-red-600 hover:bg-red-700 hover:border-red-700 hover:text-white"
+                      : "")
+                  }
+                  onClick={handleMic}
+                >
+                  <Microphone />
+                </button>
+              </div>
+            </footer>
+          </div>
+          <div className="col-span-1 p-8">
+            Mood Report:
+            {recentEmotions.length > 0 ? (
+              <Expressions top3={recentEmotions} />
+            ) : (
+              <div className="text-xs p-3 w-full border-t border-border flex flex-col md:flex-row gap-3 mt-2">
+                <div className="w-full overflow-hidden">
+                  <div className="flex items-center justify-between gap-1 font-mono pb-1">
+                    <div className="text-md">
+                      No emotions detected. Use the microphone for a more
+                      empathetic experience!
                     </div>
                   </div>
-                );
-              }
-            })}
-            {loading && (
-              <div className="flex mb-4 cursor-pointer mx-4">
-                <div className="flex max-w-96 bg-white rounded-lg px-4 py-4 gap-3">
-                  <TypingAnimation />
                 </div>
               </div>
             )}
-          </ScrollContainer>
-        </div>
 
-        <footer className="fixed p-0 absolute bottom-0 h-20 flex items-center w-2/3 rounded-br-lg">
-          <div className="flex items-center p-4 items-center w-full">
-            <input
-              type="text"
-              placeholder="Type a message..."
-              className="w-full p-2 rounded-md border border-white bg-transparent text-white"
-              onChange={(e) => setMessage(e.target.value)}
-              value={message}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  handleSubmit();
-                }
-              }}
-            />
-            <button
-              className={
-                "bg-transparent border-white border text-white text-md px-4 py-2 ml-2 rounded-lg hover:bg-white hover:text-black duration-500" +
-                (readyState === VoiceReadyState.CONNECTED ||
-                readyState === VoiceReadyState.OPEN
-                  ? " bg-red-500"
-                  : "")
-              }
-              onClick={handleMic}
-            >
-              <Microphone />
-            </button>
+            <div className="absolute p-8 bottom-0 right-0 w-1/4">
+            <Image src="/tutor-mascot.png" alt="Tutor Mascot" width={360} height={360} className=""/>
+            </div>
           </div>
-        </footer>
+        </div>
       </div>
     </main>
   );
